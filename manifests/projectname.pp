9e3d24859f09aa6f0c1ce2c8748bddd9aea78739
root <root@puppetmon.example.com>
2017-11-20 11:50:53 +0800

    puppet files

---
+++ manifests/projectname.pp
@@ -0,0 +1,12 @@
+node "puppetmon.example.com" {
+
+file { '/root/example_file.txt':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "700",
+    content => "Congratulations!
+Puppet has created this file.
+",}
+
+} # End node puppetmon.example.com
