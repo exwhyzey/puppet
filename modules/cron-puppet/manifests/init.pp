a681db363ad3d56600a34eded2bddcfab5c6c579
root <root@puppetmon.example.com>
2017-11-20 16:37:39 +0800

    update 2

--- modules/cron-puppet/manifests/init.pp
+++ modules/cron-puppet/manifests/init.pp
@@ -1,13 +1,17 @@
 class cron-puppet {
 
-file { '/root/example_file2.txt':
-    ensure => "file",
-    owner  => "root",
-    group  => "root",
-    mode   => "700",
-    content => "Congratulations!
-Puppet has created this file.
-",}
+package { 'vim': ensure => present, allow_virtual => true, }
+package { 'curl': ensure => present, allow_virtual => true, require => Package['vim'],}
+package { 'git': ensure => present, allow_virtual => true, require => Package['curl'],}
+
+# file { '/root/example_file2.txt':
+#    ensure => "file",
+#    owner  => "root",
+#    group  => "root",
+#    mode   => "700",
+#    content => "Congratulations!
+#Puppet has created this file.
+#",}
 
 user { 'monitor':
   name	     => 'monitor',
@@ -17,10 +21,21 @@ user { 'monitor':
   home       => '/home/monitor',
 }
 
-package { 'vim': ensure => present, allow_virtual => true, }
-package { 'curl': ensure => present, allow_virtual => true, require => Package['vim'],}
-package { 'git': ensure => present, allow_virtual => true, require => Package['curl']}
+$dir1 = ['/home/monitor/', '/home/monitor/scripts','/home/monitor/src/',]
 
+file { $dir1:
+    ensure => "directory",
+    owner  => "root",
+    group  => "root",
+    mode   => "0750",
+} 
+
+cron { 'run-memory-check':
+    ensure  => present,
+    command => "/home/monitor/src/my_memory_check -c 90 -w 70 -e exwhyzey3@yahoo.com",
+    user    => root,
+    minute  => '*/2',
+}
 
 cron { 'puppet-apply':
     ensure  => present,
